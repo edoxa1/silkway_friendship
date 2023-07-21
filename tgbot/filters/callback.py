d3ca6128e0cba3ea52daae4122504b90405a8d91
edoxa1 <vip.aidyn1@mail.ru>
2023-07-21 18:27:32 +0600

    modified callback data filter

--- tgbot/filters/callback.py
+++ tgbot/filters/callback.py
@@ -6,12 +6,15 @@ from aiogram.dispatcher.filters import BoundFilter
 class CallbackFilter(BoundFilter):
     key = 'callback_data'
 
-    def __init__(self, callback_data: typing.Optional[str] = None):
+    def __init__(self, callback_data: typing.Union[str, typing.List[str]] = None):
         self.callback_data = callback_data
 
     async def check(self, obj):
         if self.callback_data is None:
             return False
 
-        return obj.data == self.callback_data
+        if isinstance(self.callback_data, str):
+            return obj.data == self.callback_data
+
+        return obj.data in self.callback_data
 
