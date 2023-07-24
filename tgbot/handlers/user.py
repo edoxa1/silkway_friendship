291991cb42b9a8750cb64124fe1514dd0b05c961
gauwuhar <gaukhartoktagulova@gmail.com>
2023-07-24 19:57:46 +0600

    added my profile and find someone buttons

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -4,25 +4,30 @@ from tgbot.keyboards import inline
 
 
 async def user_start(message: Message):
-    await message.reply("Hello, user!", reply_markup=inline.generate_startup_keyboard())
+    await message.reply(f"Hello, {message.from_user.first_name}!", reply_markup=inline.generate_startup_keyboard())
 
 async def dicer(message: Message):
     answer_dice = await message.answer_dice()
     await message.answer(text=f"{answer_dice.dice.value}", reply_markup=inline.generate_test_keyboard())
 
 async def find_someone(call: CallbackQuery):
-    await call.message.answer(call.data)
-    await call.answer("you chose shoes")
+    await call.message.answer("finding someone...", reply_markup=inline.generate_find_someone_keyboard())
+    await call.answer("you chose find someone")
 
 async def my_profile(call:CallbackQuery):
-    await call.message.answer(call.data)
-    await call.answer("you chose tshirts")
+    await call.message.answer("your profile:\n", reply_markup=inline.generate_profile_keyboard())
+    await call.answer("you chose my profile")
+
+async def deactivate(call:CallbackQuery):
+    await call.message.answer("are you sure?", reply_markup=inline.generate_deactivate_keyboard())
 
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*")
     dp.register_message_handler(dicer, commands=["roll"])
     dp.register_callback_query_handler(find_someone, callback_data="find_someone")
     dp.register_callback_query_handler(my_profile, callback_data="my_profile")
+    dp.register_callback_query_handler(deactivate, callback_data="deactivate")
+
 
 
 
