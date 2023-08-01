8ebc3055b4abef4a2bad44512fb32591d567129c
edoxa1 <vip.aidyn1@mail.ru>
2023-08-02 00:12:39 +0600

    new database commands and docs

--- tgbot/services/database.py
+++ tgbot/services/database.py
@@ -1,7 +1,8 @@
+from random import randint
 import sqlite3
-from typing import Tuple, Any
 
 from tgbot.models.Enums import University
+from tgbot.models.User import User
 
 
 class Database:
@@ -9,27 +10,48 @@ class Database:
         self.connection = sqlite3.connect(path)
         self.cursor = self.connection.cursor()
         self.table_name = "users"
+        # self.__create_table()
 
-    def user_exists(self, user_id: int):
-        self.cursor.execute(f"SELECT DISTINCT * FROM {self.table_name} "
-                            f"WHERE user_id=?", (user_id,))
-
-        data = self.cursor.fetchone()
-        return bool(data)
-
-    def get_user(self, user_id: int) -> Tuple[Any]:
-        self.cursor.execute(f"SELECT DISTINCT * FROM {self.table_name} "
-                            f"WHERE user_id=?", (user_id,))
-
-        data = self.cursor.fetchone()
-        return data
-
-    def add_user(self, user_id: int, username: str, nickname: str, university: University) -> bool:
-        if not self.user_exists(user_id):
-            self.cursor.execute(f"INSERT INTO {self.table_name} (user_id, username, nickname, university) "
-                                "VALUES (?, ?, ?, ?)", (user_id, username, nickname, university.value))
-            self.connection.commit()
+    def user_exists(self, user_id: int) -> bool:
+        try:
+            self.get_user(user_id)
             return True
+        except:
+            return False
+
+    def get_user(self, user_id: int) -> User:
+        self.cursor.execute(f"SELECT DISTINCT * FROM {self.table_name} "
+                            f"WHERE user_id=?", (user_id, ))
+
+        data = self.cursor.fetchone()
+        if data:
+            return User(user_id, University(data[2]), data[3], data[4], data[5])
+
+        raise Exception("User not found")
+
+    def get_user_verification_code(self, user_id: int) -> int:
+        self.cursor.execute(f"SELECT verification_code FROM {self.table_name} "
+                            f"WHERE user_id=?", (user_id, ))
+        data = self.cursor.fetchone()
+        if data:
+            return int(data[0])
+
+        return None
+
+    def add_user(self, user_id: int, university: University) -> User:
+        """Add user if not exists
+        :param user_id: user id
+        :param university: University(Enum) -> NU = 1, AITU = 2
+
+        :return: tgbot.models.User
+        """
+        if not self.user_exists(user_id):
+            code = randint(10 ** 5, 10 ** 6 - 1)
+            self.cursor.execute(f"INSERT INTO {self.table_name} "
+                                f"(user_id, university, is_verified, is_banned, verification_code) "
+                                "VALUES (?, ?, ?, ?, ?)", (user_id, university.value, False, False, code))
+            self.connection.commit()
+            return User(user_id, university, False, False, code)
 
         raise Exception("User already exists")
 
@@ -38,6 +60,20 @@ class Database:
         for row in self.cursor.fetchall():
             print(row)
 
+    def get_count(self) -> int:
+        self.cursor.execute(f"SELECT COUNT(user_id) FROM {self.table_name};")
+        return int(self.cursor.fetchone()[0])
+
     def close_connection(self):
         """Close the connection"""
         self.connection.close()
+
+    def __create_table(self):
+        self.cursor.execute(f"CREATE TABLE {self.table_name} ("
+                            f"id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            f"user_id INTEGER NOT NULL,"
+                            f"university INTEGER,"
+                            f"is_verified BOOL,"
+                            f"is_banned BOOL,"
+                            f"verification_code INTEGER);")
+        self.connection.commit()
