d199b6759ea60c44bef9da8e4c4c484834599f9e
edoxa1 <vip.aidyn1@mail.ru>
2023-08-02 00:08:56 +0600

    fixed command imports

--- tgbot/handlers/register.py
+++ tgbot/handlers/register.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher
 
-from tgbot.handlers.user.register_user import register_user
-
+from tgbot.handlers.user.start import register_user
+from tgbot.handlers.admin.admin import register_admin
 
 def register_message_handlers(dp: Dispatcher):
+    register_admin(dp)
     register_user(dp)
--- tgbot/handlers/user/start.py
+++ tgbot/handlers/user/start.py
@@ -6,18 +6,19 @@ from tgbot.models.Enums import University
 
 
 async def user_start(message: Message):
-    await message.reply("Hello, user!", reply_markup=inline.generate_startup_keyboard())
+    await message.reply(f"Hello, {message.from_user.first_name}!", reply_markup=inline.generate_startup_keyboard())
 
 
 async def find_someone(call: CallbackQuery):
-    await call.message.answer(call.data)
     db = call.bot['config'].db  # access DB using bot['config'] (see tgbot/config.py)
-    db.add_user(call.from_user.id, call.from_user.username, "abebus", University.AITU)
-    await call.message.answer(f"Successfully registered {call.from_user.username}")
-    user_tuple = db.get_user(call.from_user.id)
-    await call.message.answer(user_tuple)
+    if not db.user_exists(call.from_user.id):
+        user = db.add_user(call.from_user.id, University.AITU)
+        await call.message.answer(user.info())
+        return
+
     await call.message.answer(f"Person id {call.from_user.id} exists: {db.user_exists(call.from_user.id)}")
-    await call.message.answer(f"Person id 0 exists: {db.user_exists(0)}")
+    user = db.get_user(call.from_user.id)
+    await call.message.answer(user.info())
     # see tgbot/services/database.py to check all methods
 
 
@@ -26,4 +27,13 @@ async def my_profile(call: CallbackQuery):
     await call.answer("you chose tshirts")
     db = call.bot['config'].db
     db.print_all()
+    code = db.get_user_verification_code(call.from_user.id)
+    if code:
+        await call.message.answer(text=code)
+
+
+def register_user(dp: Dispatcher):
+    dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_callback_query_handler(find_someone, callback_data="find_someone")
+    dp.register_callback_query_handler(my_profile, callback_data="my_profile")
 
