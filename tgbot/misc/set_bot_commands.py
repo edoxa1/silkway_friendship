176dba5eac0b2208db96a51ea702bc1221a90bb8
edoxa1 <vip.aidyn1@mail.ru>
2023-08-03 13:29:49 +0600

    Added separate commands for users and admins

--- tgbot/misc/set_bot_commands.py
+++ tgbot/misc/set_bot_commands.py
@@ -1,9 +1,23 @@
 from aiogram import Dispatcher
-from aiogram.types import BotCommand
+from aiogram.types import BotCommand, BotCommandScope, BotCommandScopeChat
 
 
 async def set_default_commands(dp: Dispatcher):
+    await set_user_commands(dp)
+    await set_admin_commands(dp)
+
+
+async def set_user_commands(dp: Dispatcher):
     await dp.bot.set_my_commands([
         BotCommand("start", "Start bot"),
-        BotCommand("roll", "Roll a dice")
     ])
+
+
+async def set_admin_commands(dp: Dispatcher):
+    admins = dp.bot['config'].tg_bot.admin_ids
+    for admin_id in admins:
+        await dp.bot.set_my_commands([
+            BotCommand("start", "Start bot"),
+            BotCommand("verify", "verify user")],
+            scope=BotCommandScopeChat(admin_id)
+        )
